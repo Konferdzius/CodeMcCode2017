d36c8f65003138da6e213a726f6308930ad5694e
Konferdzius <alwayspizzatime@gmail.com>
2017-02-23 21:35:38 +0100

    functionality

--- HashCode2017/src/SolutionSaving/SolutionGatherer.java
+++ HashCode2017/src/SolutionSaving/SolutionGatherer.java
@@ -24,19 +24,26 @@ public class SolutionGatherer {
 			if(!cache.videos.isEmpty()) {
 				String output = "";
 				
-				//save cache Number!
+				//save cache ID!
 				output = output.concat(String.valueOf(i));
 				output = output.concat(" ");
 				
-				//save every video in cache
+				//save every video-id 
 				for(Video video: cache.videos){
-					
+					output = output.concat(String.valueOf(video.id)).concat(" ");
 				}
+				//delete last unused space and make new line
+				output.substring(0, output.length()-1);
+				output.concat("/n");
 				
 				cacheToString.add(output);
 				amountOfCaches++;
 			}
 		}
+		
+		//save in solution Object
+		solution.amountOfCaches = amountOfCaches;
+		solution.cacheServers = cacheToString;
 		
 		return solution;
 	}
