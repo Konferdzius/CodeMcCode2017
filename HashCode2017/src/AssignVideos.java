b0e5439cf108845dc039584c454919d72271432b
Michael Nöth <michael@mnoeth.cc>
2017-02-23 19:43:31 +0100

    implement video request count

---
+++ HashCode2017/src/AssignVideos.java
@@ -0,0 +1,12 @@
+
+public class AssignVideos {
+	private final Environment env;
+	
+	public AssignVideos(Environment environment){
+		env = environment;
+	}
+	
+	public void assignVideos(){
+		
+	}
+}
