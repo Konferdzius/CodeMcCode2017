8c2e87d2c0cb60f6c53822906f0c5e0e5cb62113
Michael Nöth <michael@mnoeth.cc>
2017-02-23 20:10:08 +0100

    stuff

--- HashCode2017/src/model/AssignVideos.java
+++ HashCode2017/src/model/AssignVideos.java
@@ -3,15 +3,27 @@ package model;
 public class AssignVideos {
 	private final Environment env;
 	private final int[] totalVideoRequests;
+	private final int[] endpointTotalRequests;
 	
 	public AssignVideos(Environment environment){
 		env = environment;
 		
 		totalVideoRequests = new int[env.videos.size()];
+		endpointTotalRequests = new int[env.caches.size()];
 		
 		for(int i=0; i<env.videos.size(); i++){
 			totalVideoRequests[env.videos.get(i).id] = getTotalRequestCountForVideoWithID(env.videos.get(i).id);
 		}
+	}
+	
+	private int getTotalRequestCountForCacheWithID(int id){
+		int count = 0;
+		
+		for(int i = 0; i < env.endpoints.size(); i++){
+			count += env.endpoints.get(i).videoRequestCount[id];
+		}
+		
+		return count;
 	}
 	
 	private int getTotalRequestCountForVideoWithID(int id){
