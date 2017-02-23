65028e4c0c445d986ef402ac1a67e83d7255293a
Alexander Albert <alexander1.albert@stud.uni-bamberg.de>
2017-02-23 21:27:31 +0100

    testing

--- HashCode2017/src/Main.java
+++ HashCode2017/src/Main.java
@@ -8,10 +8,10 @@ import model.Environment;
 public class Main {
 
 	public static void main(String[] args) {
-		optimize("kittens.in");
-		optimize("me_at_the_zoo.in");
-		optimize("trending_today.in");
-		optimize("video_worth_spreading.in");
+		optimize("./src/kittens.in");
+		//optimize("me_at_the_zoo.in");
+		//optimize("trending_today.in");
+		//optimize("video_worth_spreading.in");
 	}
 	
 	private static void optimize(String filename) {
@@ -19,7 +19,7 @@ public class Main {
 		
 		Parser parser = new Parser();
 		
-		Environment env = parser.ParsFile(file);
+		Environment env = parser.parsFile(file);
 		new AssignVideos(env);
 		
 		SolutionGatherer gatherer = new SolutionGatherer(env);		
