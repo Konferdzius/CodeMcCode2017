50b00f792cba36f22afa311bea2a427bf6c4e93e
Konferdzius <alwayspizzatime@gmail.com>
2017-02-23 21:12:38 +0100

    fuck

--- HashCode2017/src/Main.java
+++ HashCode2017/src/Main.java
@@ -1,9 +1,28 @@
+import java.io.File;
+
+import SolutionSaving.SolutionGatherer;
+import logic.parser.Parser;
+import model.AssignVideos;
+import model.Environment;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		optimize("kittens.in");
+		optimize("me_at_the_zoo.in");
+		optimize("trending_today.in");
+		optimize("video_worth_spreading.in");
 	}
-
+	
+	private static void optimize(String filename) {
+		File file = new File(filename);
+		
+		Parser parser = new Parser();
+		
+		Environment env = parser.ParsFile(file);
+		new AssignVideos(env);
+		
+		SolutionGatherer gatherer = new SolutionGatherer(env);		
+		gatherer.gatherSolution();
+	}
 }
